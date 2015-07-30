8a1eab3d70bfdecf6b7ba40c7e5316df0764a36d
luyang <349989153@qq.com>
2015-07-30 17:38:49 +0800

    upload

--- upload.js
+++ upload.js
@@ -47,4 +47,34 @@ http.createServer(function(req,res){
 
 			break;
 	}
+
+	var chunks = [];
+	var size = 0;
+	req.on("data",function(chunk){
+		chunks.push(chunk);
+		size+=chunk.length;
+	});
+
+	req.on("end",function(){
+		var buffer = Buffer.concat(chunks,size);
+		if(!size){
+			res.writeHead(404);
+			res.end("");
+			return;
+		}
+
+		var rems = [];
+
+		for(var i = 0,l = buffer.length;i<l;i++){
+			var v = buffer[i];
+			var v2 = buffer[i+1];
+			if(v==13 && v2==10 ){
+				rems.push(i);
+			}
+		}
+
+		var picmsg_1 = buffer.slice(rems[0]+2,rems[1]).toString();
+		var filename = picmsg_1.match(/filename=".*"/g)[0].split
+	});
+
 }).listen(8000);
